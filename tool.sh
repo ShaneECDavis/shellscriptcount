61f00e75e27ec9fd20b11e6c0b733e3edb184e6a
Shane EC Davis <shaneecdavis7@gmail.com>
2019-02-16 10:32:43 -0600

    finished

--- tool.sh
+++ tool.sh
@@ -7,6 +7,9 @@
 # $answer - stores input from user
 # $count - stores how many lines in given file
 # $changes - stores what changes has been made display old file tab new filename and then new line
+# $oldmem - stores last line of the .memory file if there is one
+# $oldcount - stores the second value of oldmem array which olds the line count from previous file
+# $oldstr - olds oldcount and has message set to be displayed
 
 # Directory name given as comman line argument
 if [ $# -lt 1  ]; then
@@ -28,6 +31,10 @@ fi
 # loope over file - For each file ask user whether to count lines in a file
 # `` <- back tics means run this on command line then spit it out to be used in this scope. So lists will hold ls results as if ran on command line itself
 list=`ls "$1"`
+
+# make sure memory file exists update time stamps, if does not exists create file. This is file is hidden.
+touch ~/.memory
+
 # this loops over $list with access to each part under file which you named. Similar to array.forEach(item => {}) item = file in this scope
 for file in $list; do
 
@@ -43,8 +50,20 @@ for file in $list; do
         # cat command gets name of file and the '|' causes what is given back to be used for wc -l. saves it all to count.
         count=`cat "$1"/$file | wc -l`
 
-        # If count is different from a previous count, also display old count
-        echo "$file has $count lines"
+        #get old count grep search for name of the file in .memory then after finding it, pipes it to get the last line in the file 'tail -l'
+        oldmem=`grep "$1/$file" ~/.memory | tail -l`
+        oldstr=""
+
+        if [ ! "$oldmem" = "" ]; then
+            array=($oldmem)
+            oldcount=${array[1]}
+            if [ "$oldcount" != "$count" ]; then
+                oldstr="(old: $oldcount)"
+            fi
+        fi
+        echo "$file has $count lines $oldstr"
+        # after getting the count we are going to place our results into the .memory file to be used if we run script again
+        echo "$1/$file $count" >> ~/.memory
 
     else
         # To rename file
@@ -54,3 +73,8 @@ for file in $list; do
 # one ends the loop or in this case for just reads who inputs or data from $variable sent in
 done
 # Print list of files that were renamed
+echo "Renamed files: "
+echo -e "Before\t\tAfter"
+echo -e "$changes"
+
+
